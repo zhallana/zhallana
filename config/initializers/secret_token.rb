2e4ca0aa41e55e84a9e8b44c07bd120949409ddc
zhallana <zhallana@gmail.com>
2016-09-08 13:15:50 +0300

    new try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zhallana::Application.config.secret_token = '552b2f9fcc8854868abb47e8ef2d2b8af015e8a891038f9c528488da0818bece59909117792b0c602f3a5c982ec904f379fad13a2e18b82046b1ef20fbb0a761'
